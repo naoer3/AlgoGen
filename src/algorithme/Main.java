fbc5441036dadaec961aabc85e9d1e4ad915cc2e
Mathilde Paris <mathilde.paris1@laposte.net>
2019-01-14 10:47:39 +0100

    Programmation du main

--- src/algorithme/Main.java
+++ src/algorithme/Main.java
@@ -1,37 +1,38 @@
 package algorithme;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class Main {
 	
-	private static Population<Double> myPop =null;
-	private static SelectionMethode<Double> myLoterie=null;
-	//private static Individu<Double> typeIndividu =null;
-
 	public static void main(String[] args) {
 		
-		//setMyPop(new Population(100));
-		//setMyLoterie(new LoterieStrategy(10));
-		//setMyLoterie(new ElitisteStrategy(5));
-		setMyLoterie(new TournoiStrategy<Double>(5));
-		myLoterie.methodeSelection(myPop);
-		System.out.println(myPop.toString());
+		Function<Individu<Double>,Double> fct_eval =  (indiv) -> {
+			Double x = indiv.getGenes().get(0);
+			Double y = (2 * x) + 3;
+			return y;
+		};
+		
+		Supplier<Individu<Double>> fct_crea =  () -> {
+			Random r = new Random();
+			//x compris entre 0 et 100
+			double gene = 0 + (100 - 0) * r.nextDouble();
+			List<Double> genes = new ArrayList<>();
+			genes.add(gene);
+			Individu<Double> indiv = new Individu<>(genes);
+			return indiv;
+		};
+		
+		int taille_pop = 100;
+		int selection_parent = 0;
+		int selection_population = 1;
+		double prob_mutation = 0.03;
+		double prob_croisement = 0.2;
+		
+		Algorithme<Double> algo = new Algorithme<>(taille_pop, selection_parent, selection_population, prob_mutation, prob_croisement, fct_crea, fct_eval);
+		algo.LancerAlgorithme();
 	}
-
-	public static Population<Double> getMyPop() {
-		return myPop;
-	}
-
-	public static void setMyPop(Population<Double> myPop) {
-		Main.myPop = myPop;
-	}
-
-
-	public static SelectionMethode<Double> getMyLoterie() {
-		return myLoterie;
-	}
-
-	public static void setMyLoterie(SelectionMethode<Double> myLoterie) {
-		Main.myLoterie = myLoterie;
-	}
-
 }
